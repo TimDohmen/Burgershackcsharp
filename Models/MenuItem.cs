b5bb92514acd8583aaa320ca56fafc3d4648cf3d
Tim Dohmen <dohmentim@yahoo.com>
2019-10-04 16:17:56 -0600

    controllers and vericves

--- Models/MenuItem.cs
+++ Models/MenuItem.cs
@@ -1,10 +1,20 @@
 namespace burgersack.Models
 {
-  public class MenuItem
+  public abstract class MenuItem
   {
     public string Title { get; set; }
     public double Price { get; set; }
     public string Description { get; set; }
+
+    public virtual string GetTemplate()
+    {
+      string template = $@"
+Item: {Title}
+Cost: {Price:c}
+Description: {Description}
+";
+      return template;
+    }
 
     public MenuItem(string title, double price, string desc)
     {
